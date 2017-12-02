39fbbaa86701fade39ecbd6ce4825411c67be019
John Kwon <john.wb.kwon@gmail.com>
2017-12-02 15:33:23 -0500

    Removed city column from CSV output

--- js/makeCSV.js
+++ js/makeCSV.js
@@ -8,7 +8,6 @@ let headers = [ ['id',
         'age',
         'sex',
         'state',
-        'city',
         'income',
         'insuredAmount',
         'height',
@@ -40,35 +39,34 @@ let data = {};
 participant.getParticipant(process.argv[2], function (participants) {
     for (let i=0; i<participants.length; i++) {
         let cur = participants[i];
-        let p = Array(26).fill(0);
+        let p = Array(25).fill(0);
         p[0] = cur.id;
         p[1] = cur.DOB;
         p[2] = cur.sex;
         p[3] = cur.state;
-        p[4] = cur.city;
         data[cur.id] = p;
     }
 
     for (let pID in data) {
         pDetails.getDetails(pID, function(detail) {
-            data[pID][5] = detail.ANNUAL_INCOME;
-            data[pID][6] = detail.OPTIONAL_INSURED;
-            data[pID][7] = detail.HEIGHT;
-            data[pID][8] = detail.WEIGHT;
-            data[pID][9] = detail.TOBACCO;
-            data[pID][10] = detail.MARITAL_STATUS
+            data[pID][4] = detail.ANNUAL_INCOME;
+            data[pID][5] = detail.OPTIONAL_INSURED;
+            data[pID][6] = detail.HEIGHT;
+            data[pID][7] = detail.WEIGHT;
+            data[pID][8] = detail.TOBACCO;
+            data[pID][9] = detail.MARITAL_STATUS
             if (detail.PRE_CONDITIONS) {
                 let conditions = JSON.parse(detail.PRE_CONDITIONS);
                 for (let i=0; i<conditions.length; i++) {
                     let c = conditions[i];
                     if (c.Risk_factor === "Low") {
                         // data[pID].push(-1);
-                        data[pID][i+11] = -1;
+                        data[pID][i+10] = -1;
                     } else if (c.Risk_factor === "Medium") {
-                        data[pID][i+11] = 0;
+                        data[pID][i+10] = 0;
                         // data[pID].push(0);
                     } else {
-                        data[pID][i+11] = 1;
+                        data[pID][i+10] = 1;
                         // data[pID].push(1);
                     }
                 }
@@ -78,11 +76,11 @@ participant.getParticipant(process.argv[2], function (participants) {
                 }
             }
             quote.getQuote(pID, function(quote) {
-                data[pID][21] = quote.BRONZE;
-                data[pID][22] = quote.SILVER;
-                data[pID][23] = quote.GOLD;
-                data[pID][24] = quote.PLATINUM;
-                data[pID][25] = quote.PURCHASED;
+                data[pID][20] = quote.BRONZE;
+                data[pID][21] = quote.SILVER;
+                data[pID][22] = quote.GOLD;
+                data[pID][23] = quote.PLATINUM;
+                data[pID][24] = quote.PURCHASED;
                 let input = [data[pID]];
                 fs.appendFile('data/'+ process.argv[3] + '.csv', data[pID].join(',') + '\n');
             });
