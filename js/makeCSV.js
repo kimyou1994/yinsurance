b7a3953fc541385d16f545ec97ed8a46fe1df51f
Will Liu <will.r.liu@gmail.com>
2017-12-03 05:35:23 -0500

    Add more code to parse data into more relevant categories

--- js/makeCSV.js
+++ js/makeCSV.js
@@ -25,25 +25,32 @@ let headers = [ ['id',
         'hiv',
         'hemorrhage',
         'bronze',
+        's-b',
         'silver',
+        'g-s',
         'gold',
+        'p-s',
         'platinum',
-        'purchased'] ];
+        'purchased',
+        'stateIncome',
+        'diffIncome',
+        'bmi'] ];
 
-csv(headers, function (err, res) {
-    fs.writeFile('data/' + process.argv[3] + '.csv', res);
-});
+// csv(headers, function (err, res) {
+//     fs.writeFile('data/' + process.argv[3] + '.csv', res);
+// });
 
 let data = {};
 
 participant.getParticipant(process.argv[2], function (participants) {
     for (let i=0; i<participants.length; i++) {
         let cur = participants[i];
-        let p = Array(25).fill(0);
+        let p = Array(31).fill(0);
         p[0] = cur.id;
         p[1] = cur.DOB;
         p[2] = cur.sex;
         p[3] = cur.state;
+        p[28] = cur.state;
         data[cur.id] = p;
     }
 
@@ -53,6 +60,7 @@ participant.getParticipant(process.argv[2], function (participants) {
             data[pID][5] = detail.OPTIONAL_INSURED;
             data[pID][6] = detail.HEIGHT;
             data[pID][7] = detail.WEIGHT;
+            data[pID][30] = 703 * detail.WEIGHT / (detail.HEIGHT * detail.HEIGHT);
             data[pID][8] = detail.TOBACCO;
             data[pID][9] = detail.MARITAL_STATUS
             if (detail.PRE_CONDITIONS) {
@@ -61,26 +69,37 @@ participant.getParticipant(process.argv[2], function (participants) {
                     let c = conditions[i];
                     if (c.Risk_factor === "Low") {
                         // data[pID].push(-1);
-                        data[pID][i+10] = -1;
+                        data[pID][i+10] = 5;
                     } else if (c.Risk_factor === "Medium") {
-                        data[pID][i+10] = 0;
+                        data[pID][i+10] = 10;
                         // data[pID].push(0);
                     } else {
-                        data[pID][i+10] = 1;
+                        data[pID][i+10] = 15;
                         // data[pID].push(1);
                     }
                 }
             } else {
                 for (let i=10; i<20; i++) {
-                   data[pID][i] = -1;
+                   data[pID][i] = 0;
                 }
             }
             quote.getQuote(pID, function(quote) {
                 data[pID][20] = quote.BRONZE;
-                data[pID][21] = quote.SILVER;
-                data[pID][22] = quote.GOLD;
-                data[pID][23] = quote.PLATINUM;
-                data[pID][24] = quote.PURCHASED;
+                data[pID][21] = quote.SILVER - quote.BRONZE;
+                data[pID][22] = quote.SILVER;
+                data[pID][23] = quote.GOLD - quote.SILVER;
+                data[pID][24] = quote.GOLD;
+                data[pID][25] = quote.PLATINUM - quote.GOLD;
+                data[pID][26] = quote.PLATINUM;
+                if (quote.PURCHASED === 'Bronze') {
+                    data[pID][27] = 0;
+                } else if (quote.PURCHASED === 'Silver') {
+                    data[pID][27] = 1;
+                } else if (quote.PURCHASED === 'Gold') {
+                    data[pID][27] = 2;
+                } else {
+                    data[pID][27] = 3;
+                }
                 let input = [data[pID]];
                 fs.appendFile('data/'+ process.argv[3] + '.csv', data[pID].join(',') + '\n');
             });
